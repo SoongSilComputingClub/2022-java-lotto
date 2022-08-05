87afa4a8ac544a6f2f6e348f19b45a363e17ae05
jihong <clara5091@naver.com>
2022-08-05 15:34:50 +0900

    LottoNumber 클래스 생성

--- src/main/java/org/sscc/study/lotto/LottoNumber.java
+++ src/main/java/org/sscc/study/lotto/LottoNumber.java
@@ -1,5 +1,27 @@
 package org.sscc.study.lotto;
 
-public class LottoNumber {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+public class LottoNumber {
+    private final List<Integer> lottoNumbers;
+    private static final int MAXIMUM = 45;
+    private static final int MINIMUM = 1;
+
+    public LottoNumber() {
+        lottoNumbers = new ArrayList<>();
+        for (int i = MINIMUM; i <= MAXIMUM; i++) {
+            lottoNumbers.add(i);
+        }
+    }
+
+    private void shuffle() {
+        Collections.shuffle(lottoNumbers);
+    }
+
+    public List<Integer> getLottoNumbers(int n){
+        shuffle();
+        return lottoNumbers.subList(0, n);
+    }
 }
--- src/main/java/org/sscc/study/lotto/RandomLottoNumber.java
+++ src/main/java/org/sscc/study/lotto/RandomLottoNumber.java
@@ -1,27 +1,17 @@
 package org.sscc.study.lotto;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class RandomLottoNumber implements NumberStrategy {
-    private final List<Integer> lotteryNumbers;
+    private final LottoNumber lottoNumber;
     private static final int NUMBER = 6;
 
     public RandomLottoNumber() {
-        lotteryNumbers = new ArrayList<>();
-        for (int i = 1; i <= 45; i++) {
-            lotteryNumbers.add(i);
-        }
+        lottoNumber = new LottoNumber();
     }
 
     @Override
     public List<Integer> getNumber() {
-        shuffle();
-        return lotteryNumbers.subList(0, NUMBER);
-    }
-
-    private void shuffle() {
-        Collections.shuffle(lotteryNumbers);
+        return lottoNumber.getLottoNumbers(NUMBER);
     }
 }
