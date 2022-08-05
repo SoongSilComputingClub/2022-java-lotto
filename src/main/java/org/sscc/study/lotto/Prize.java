da92de5a8b8aea4cd3e01b601d49aa07825693ca
jihong <clara5091@naver.com>
2022-08-05 10:21:12 +0900

    숫자 표기 변경

--- src/main/java/org/sscc/study/lotto/Prize.java
+++ src/main/java/org/sscc/study/lotto/Prize.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 
 public enum Prize {
     FOURTH(3, 5000),
-    THIRD(4, 50000),
-    SECOND(5, 1500000),
-    FIRST(6, 2000000000);
+    THIRD(4, 50_000),
+    SECOND(5, 1_500_000),
+    FIRST(6, 2_000_000_000);
 
     private final int number;
     private final int prize;
