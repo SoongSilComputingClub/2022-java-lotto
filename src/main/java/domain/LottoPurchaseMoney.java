066e0871f25089ba2060253cdd64c804497aad0f
wygbsr <wygbsr117@gmail.com>
2022-08-07 19:57:44 +0900

    LottoPurchaseMoney 리팩터링

--- src/main/java/domain/LottoPurchaseMoney.java
+++ src/main/java/domain/LottoPurchaseMoney.java
@@ -3,14 +3,14 @@ package domain;
 public class LottoPurchaseMoney {
     private static final int LOTTO_PRICE = 1000;
 
-    private final int lottoPurchaseMoney;
+    private final int lottoAmount;
 
     private final int lottoInvestMoney;
 
     public LottoPurchaseMoney(int krw) {
         validateMoney(krw);
-        this.lottoPurchaseMoney = krw;
-        this.lottoInvestMoney = krw / LOTTO_PRICE * LOTTO_PRICE;
+        this.lottoAmount = krw / LOTTO_PRICE;
+        this.lottoInvestMoney = lottoAmount * LOTTO_PRICE;
     }
 
     private void validateMoney(int krw) {
@@ -20,7 +20,7 @@ public class LottoPurchaseMoney {
     }
 
     public int getAmount() {
-        return lottoPurchaseMoney / LOTTO_PRICE;
+        return lottoAmount;
     }
 
     public int getInvestMoney() {
