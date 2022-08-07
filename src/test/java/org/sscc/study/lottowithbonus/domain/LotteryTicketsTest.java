06e2f1d3079f3567e7f2a51fc6dea71f6ee037b5
jihong <clara5091@naver.com>
2022-08-08 08:12:39 +0900

    test code 추가

---
+++ src/test/java/org/sscc/study/lottowithbonus/domain/LotteryTicketsTest.java
@@ -0,0 +1,39 @@
+package org.sscc.study.lottowithbonus.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class LotteryTicketsTest {
+    @Test
+    @DisplayName("당첨번호 입력 시 번호가 일치하는 지 확인")
+    public void testWinning() {
+        LotteryTickets lotteryTickets = new LotteryTickets();
+        lotteryTickets.addLotteryTicket(new StaticLottoNumberGenerator());
+        List<MatchData> matchData = lotteryTickets.
+                duplicateNumbers(new WinningNumbers("1, 2, 3, 4, 5, 6", 7));
+        assertThat(matchData.get(0)).isEqualTo(new MatchData(6, false));
+    }
+
+    @Test
+    @DisplayName("2등(5개-보너스볼)이 일치되는 지 확인")
+    public void testSecond(){
+        LotteryTickets lotteryTickets = new LotteryTickets();
+        lotteryTickets.addLotteryTicket(new StaticLottoNumberGenerator());
+        List<MatchData> matchData = lotteryTickets.
+                duplicateNumbers(new WinningNumbers("1, 2, 3, 4, 5, 10", 6));
+        assertThat(matchData.get(0)).isEqualTo(new MatchData(5, true));
+    }
+
+    @Test
+    @DisplayName("로또 번호가 문자열 형식에 맞게 출력되는 지 확인")
+    public void testNumberString() {
+        LotteryTickets lotteryTickets = new LotteryTickets();
+        lotteryTickets.addLotteryTicket(new StaticLottoNumberGenerator());
+        String result = lotteryTickets.getString();
+        assertThat(result).isEqualTo("[1, 2, 3, 4, 5, 6]");
+    }
+}
---
+++ src/test/java/org/sscc/study/lottowithbonus/domain/WinningStatTest.java
@@ -0,0 +1,38 @@
+package org.sscc.study.lottowithbonus.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class WinningStatTest {
+
+    @Test
+    @DisplayName("수익률 계산 테스트")
+    public void testPortfolio() {
+        WinningStat winningStat = new WinningStat();
+        winningStat.add(new MatchData(3, true));
+        winningStat.add(new MatchData(3, false));
+        winningStat.add(new MatchData(5, true));
+        assertThat(winningStat.portfolio(10000)).isEqualTo(3001.00);
+    }
+
+    @Test
+    @DisplayName("당첨 개수 확인 테스트")
+    public void testNumber() {
+        WinningStat winningStat = new WinningStat();
+        winningStat.add(new MatchData(6, false));
+        winningStat.add(new MatchData(3, true));
+        winningStat.add(new MatchData(2, true));
+        assertThat(winningStat.get(3)).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("5개 일치 시 보너스볼 일치에 대해 분리되는 지 확인")
+    public void testSecond(){
+        WinningStat winningStat = new WinningStat();
+        winningStat.add(new MatchData(5, false));
+        winningStat.add(new MatchData(5, true));
+        assertThat(winningStat.get(5, true)).isEqualTo(1);
+    }
+}
